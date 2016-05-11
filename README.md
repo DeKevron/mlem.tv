53efc439910338c6da2264db9b2c6820b1779d06
Kevin Brooks <kev@dekevron.com>
2016-05-11 11:45:27 -0400

    Added tuning static and hash loader

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 A celebration of the sound affect made when a tongue exits and re-enters the mouth of cute animals. 
 
+See it in action: <http://www.mlem.tv/>
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,27 @@
-var imageSearch;
+// slows the static refresh rate by 0.5
 var toggle = true;
+// show static?
 var tvStatic = true;
-var tvChannel = 2;
-var tvChannelMax = 20; 
+// is the TV paused on a channel?
 var tvPause = false;
+// Current Channel - start it at VHF 2 :D
+var tvChannel = 2;
+// min and max chanels to cycle through
+var tvChannelMin = 2; 
+var tvChannelMax = 20; 
+
+var userPromptCount = 5;
+var userPrompt = true;
 
 $(document).ready(function() {
-	// SRC: http://jsfiddle.net/AbdiasSoftware/FrMNL/
+	// Set up the canvas for the static effect
 	var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d');
 
-	// closer to analouge appearance
+	// Closer to analouge appearance
 	canvas.width = canvas.height = 256;
 
+	// Resize the canvas
 	function resize() {
 	    canvas.style.width = window.innerWidth + 'px';
 	    canvas.style.height = window.innerHeight + 'px';
@@ -21,6 +30,18 @@ $(document).ready(function() {
 	resize();
 	window.onresize = resize;
 
+	// Loads channel from hash link
+	function loadHash() {
+		var hash = window.location.hash.substring(1);
+		if(hash.indexOf('channel') >= 0) {
+			//tvPause = true;
+			tvChannel = parseInt(hash.replace('channel', ''));
+			$('#tv-channel').text('CH '+padZero(tvChannel, 3));
+			toggleSearch();
+		}
+	}
+
+	// updates the channel scan if the TV isn't paused on a channel
 	function updateChannel() {
 		setInterval(function() {
 			if(!tvPause) {
@@ -34,15 +55,29 @@ $(document).ready(function() {
 				window.location.hash = '';
 			} else {
 				window.location.hash = 'channel'+padZero(tvChannel, 3);
+				userPrompt = false;
+			}
+
+			if(userPromptCount > 0) {
+				userPromptCount--;
+			} 
+
+			if(userPromptCount === 0 && userPrompt) {
+				$('#tv-prompt').show();
+			} else if(!userPrompt) {
+				$('#tv-prompt').hide();
 			}
 		}, 575);
 	}
 
+	// pads the channel number with zeros to closer match appearance of old TVs
 	function padZero(num, places) {
 		var zero = places - num.toString().length + 1;
 		return Array(+(zero > 0 && zero)).join("0") + num;
 	}
 
+	// generate canvas static noise
+	// Borrowed from: http://jsfiddle.net/AbdiasSoftware/FrMNL/
 	function noise(ctx) {
 	    
 	    var w = ctx.canvas.width,
@@ -58,48 +93,82 @@ $(document).ready(function() {
 	    ctx.putImageData(idata, 0, 0);
 	}
 
+	// generate tuning effect
+	// Borrowed from: http://jsfiddle.net/AbdiasSoftware/t4Pvq/
+	function interference(ctx) {
+    
+	    var w = ctx.canvas.width,
+	        h = ctx.canvas.height,
+	        idata = ctx.getImageData(0, 0, w, h),
+	        buffer32 = new Uint32Array(idata.data.buffer),
+	        len = buffer32.length,
+	        i = 0,
+	        pr = 456 * Math.random(),
+	        prs = 716 * Math.random();;
+
+	    for(; i < len;) {
+	        buffer32[i++] = ((pr % 255)|0) << 24;
+	        pr += prs * 1.2;
+	    }
+	    
+	    ctx.putImageData(idata, 0, 0);
+	}
+
+	function tuneChannel() {
+		$('img#mlemgif').attr('src', 'images/mlem'+padZero(tvChannel,3)+'.gif').on('load', function() {
+			$(this).show();
+			$('#tv-loading').hide();
+			// setTimeout(function() {
+			// 	$('#tv-channel').hide();
+			// }, 3000);
+		});
+	}
+
+	function detuneChannel() {
+		$('img#mlemgif').attr('src', 'images/blank.gif').hide();
+		$('#tv-loading').hide();
+		$('#tv-searching').show();
+	}
+
+	// toggles the Search/Pause mode
 	function toggleSearch() {
 		tvPause = !tvPause;
 		$('#tv-channel').show();
 		$('#tv-loading').show();
 		$('#tv-searching').hide();
 		if(tvPause) {
-			$('img#mlemgif').attr('src', 'images/mlem'+padZero(tvChannel,3)+'.gif').on('load', function() {
-				$(this).show();
-				$('#tv-loading').hide();
-				// setTimeout(function() {
-				// 	$('#tv-channel').hide();
-				// }, 3000);
-			});
+			tuneChannel();
 		} else {
-			$('img#mlemgif').attr('src', 'images/blank.gif').hide();
-			$('#tv-loading').hide();
-			$('#tv-searching').show();
+			detuneChannel();
 		}
-		
-		
 	}
 
-	//added toggle to get 30 FPS instead of 60 FPS
+	// added toggle to get 30 FPS instead of 60 FPS
 	(function loop() {
 		toggle = !toggle;
 	    if (toggle) {
 	        requestAnimationFrame(loop);
 	        return;
 	    }
-	    noise(ctx);
+	    if(!tvPause) {
+	    	noise(ctx);
+	    } else {
+	    	interference(ctx);
+	    }
 	    requestAnimationFrame(loop);
 	})();
 
+	// start channel cycle
 	updateChannel();
+	// check hash and load channel
+	loadHash();
 
+	// bind click/touch/keyboard interactions
 	$('body').keyup(function(e){
 	   if(e.keyCode == 32){
 	       // user has pressed space
 	       toggleSearch();
 	   }
 	}).bind('click', toggleSearch);
-
-
 
 });
